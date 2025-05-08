e57c855dbe61f10547a97a638b8fd472e9c89f37
Nhan Lam <lamnhanx512@gmail.com>
2025-05-08 15:50:35 +0700

    Enhancing blog

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ A basic blog application built with Django.
 - Defined model: 'Post'
 - Configured admin dashboard for blog management
 - Created superuser and tested admin panel
+- Using and modifying cannonical Urls for model and modifiying the URL patterns
+- Adding pagination and handling pagination errors
+- Recommending posts by email:
+    + Creating forms with Django
+    + Handling forms in views
+    + Using environment variables to define the SMTP configuration
+    + Sending email in views
+    + Rendering forms in template
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", email="test@email.com", password="test123"
+        )
+        cls.post = Post.objects.create(
+            title="A title", body="Body content", author=cls.user
+        )
+
+    def test_post_model(self):
+        self.assertEqual(self.post.title, "A title")
+        self.assertEqual(self.post.body, "Body content")
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(str(self.post), "A title")
